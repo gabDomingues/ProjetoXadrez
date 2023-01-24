55e1bf80dca90520bb93ed1da940214fad9a0d64
gabDomingues <gabrieldomingues123@hotmail.com.br>
2023-01-24 08:35:36 -0300

    Classe Posicao criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Posicao p1 = new Posicao(3,5);
+		System.out.println(p1);
 
 	}
 
